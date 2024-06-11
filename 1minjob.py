27656a0205ff1d7ce46285b07a1250e69c0b624c
vaibhav.10714747@ltimindtree.com <vaibhav.10714747@ltimindtree.com>
2024-06-11 08:57:24 +0000

    auto save by knights-watch

---
+++ 1minjob.py
@@ -0,0 +1,8 @@
+import time
+print(" 1min  Testing long job started ")
+
+time.sleep(60)
+
+print(" 1min Testing long job ended ")
+print(" 1min Testing long job ended11111111111222222222223333333333333333 ")
+print(" 1min Testing done ")
